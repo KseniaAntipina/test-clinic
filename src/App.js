500ca9a43c8e965f236f8117bb39203194fe0453
Kseniya Antipina <karinpark@mail.ru>
2020-10-05 00:03:44 +0300

    sidebar done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Route} from "react-router-dom";
+import HeaderContainer from "./components/Header/HeaderContainer";
+import Navbar from "./components/Navbar/Navbar";
+import ProfileContainer from "./components/Profile/ProfileContainer";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = (props) => {
+
+    return (
+        <BrowserRouter>
+            <div className='app-wrapper'>
+                <HeaderContainer/>
+                <Navbar/>
+                <div className='app-wrapper-content'>
+                    <Route path="/profile" render={
+                        () => <ProfileContainer/>}/>
+                </div>
+            </div>
+        </BrowserRouter>);
 }
 
 export default App;
---
+++ src/components/Profile/ProfileContainer.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Profile from "./Profile";
+
+
+class ProfileContainer extends React.Component  {
+
+    render () {
+        return (
+            <div>
+                <Profile/>
+            </div>
+        )
+    }
+
+}
+
+export default ProfileContainer;
