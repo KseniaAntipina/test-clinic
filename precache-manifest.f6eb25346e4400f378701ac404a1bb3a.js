3917c940d3435a3a17d7f3de8d3f35644a74b8bc
Kseniya Antipina <karinpark@mail.ru>
2021-03-03 17:35:01 +0300

    Updates

---
+++ precache-manifest.f6eb25346e4400f378701ac404a1bb3a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd6109400bc6aabcec87f51d8ecbbbe9",
+    "url": "/test-clinic/index.html"
+  },
+  {
+    "revision": "d5225608670757161328",
+    "url": "/test-clinic/static/css/main.29358676.chunk.css"
+  },
+  {
+    "revision": "0243b4e1e465ef6643b0",
+    "url": "/test-clinic/static/js/2.96690bfc.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/test-clinic/static/js/2.96690bfc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5225608670757161328",
+    "url": "/test-clinic/static/js/main.c092194b.chunk.js"
+  },
+  {
+    "revision": "071afcc8d9602372514a",
+    "url": "/test-clinic/static/js/runtime-main.b6099bdc.js"
+  },
+  {
+    "revision": "81f07cf97bf1fef85025cc7afd11a255",
+    "url": "/test-clinic/static/media/bell.81f07cf9.svg"
+  },
+  {
+    "revision": "3bda7792cee17c201027028d090f7040",
+    "url": "/test-clinic/static/media/dropdownMenu.3bda7792.svg"
+  },
+  {
+    "revision": "0bb5542231e56cba776203b25389ee7f",
+    "url": "/test-clinic/static/media/search.0bb55422.svg"
+  }
+]);
